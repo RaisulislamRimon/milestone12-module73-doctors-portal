e1536934aaebbd24a8ca789a6dee6cd69b7dc318
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-16 10:39:24 +0600

    uploading all resources

---
+++ README.md
@@ -0,0 +1,5 @@
+# milestone12-module73-doctors-portal
+
+### milestone12-module72-doctors-portal
+
+### milestone12-module71-doctors-portal
---
+++ doctors-portal-client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes/Router";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
